92e2a1768a9e7d0e3b579cfdd524b0990849136a
Michael Reuter <mareuter@lsst.org>
2019-11-25 12:35:12 -0700

    Add license preambles to files.

--- bin.src/standby_report.py
+++ bin.src/standby_report.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+# Developed for the LSST System Integration, Test and Commissioning Team.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the LICENSE file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# Use of this source code is governed by a 3-clause BSD-style
+# license that can be found in the LICENSE file.
+
 import sys
 
 from lsst.integration_test_reporting.bin.standbyreport import main
