56cfc186253b7a8b2d6bdcec4f1add48115f2433
Michael Reuter <mareuter@lsst.org>
2020-02-14 11:21:17 -0700

    Add CSC generation from list and make flexible source call.

--- python/lsst/integration_test_reporting/utils/csc_cls.py
+++ python/lsst/integration_test_reporting/utils/csc_cls.py
@@ -19,6 +19,9 @@ class CSC:
         self.name = name
         self.index = index
 
+    def __eq__(self, other):
+        return other.name == self.name and other.index == self.index
+
     @property
     def full_name(self):
         _csc_name = f"{self.name}"
@@ -50,3 +53,18 @@ class CSC:
                 cscs.append(CSC.from_entry(v))
 
         return cscs
+
+    @classmethod
+    def get_from_list(cls, csc_list):
+        csc_entries = csc_list.split(',')
+        cscs = []
+        for csc_entry in csc_entries:
+            cscs.append(CSC.from_entry(csc_entry))
+        return cscs
+
+    @classmethod
+    def get_from_source(cls, source):
+        if ',' in source:
+            return cls.get_from_list(source)
+        else:
+            return cls.get_from_file(source)
--- tests/test_csc.py
+++ tests/test_csc.py
@@ -7,6 +7,8 @@
 # Use of this source code is governed by a 3-clause BSD-style
 # license that can be found in the LICENSE file.
 
+import os
+
 from lsst.integration_test_reporting.utils import CSC
 
 
@@ -15,6 +17,14 @@ class TestCsc():
     def setup_class(cls):
         cls.csc1 = CSC("Environment", 1)
         cls.csc2 = CSC("ATArchiver", None)
+
+        cls.csc_list = ["Environment:1", "ATArchiver"]
+        cls.csc_obj_list = [cls.csc1, cls.csc2]
+
+    def make_csc_file(self, filename):
+        with open(filename, 'w') as ofile:
+            for csc in self.csc_list:
+                ofile.write(csc + os.linesep)
 
     def test_properties(self):
         assert self.csc1.full_name == "Environment:1"
@@ -25,3 +35,28 @@ class TestCsc():
 
         assert self.csc1.efd_topic(topic_name) == "lsst.sal.Environment.logevent_summaryState"
         assert self.csc2.efd_topic(topic_name) == "lsst.sal.ATArchiver.logevent_summaryState"
+
+    def test_from_file(self):
+        temp_file = "testing.dat"
+        self.make_csc_file(temp_file)
+
+        cscs = CSC.get_from_file(temp_file)
+        assert cscs == self.csc_obj_list
+
+        os.remove(temp_file)
+
+    def test_from_list(self):
+        truth_list = ",".join(self.csc_list)
+        cscs = CSC.get_from_list(truth_list)
+        assert cscs == self.csc_obj_list
+
+    def test_from_source(self):
+        truth_list = ",".join(self.csc_list)
+        cscs = CSC.get_from_source(truth_list)
+        assert cscs == self.csc_obj_list
+
+        temp_file = "testing1.dat"
+        self.make_csc_file(temp_file)
+        cscs = CSC.get_from_source(temp_file)
+        assert cscs == self.csc_obj_list
+        os.remove(temp_file)
